7d98a9a91eea175d911e91442cd17ecbfa579ba1
István Nagy <istvan.nagy@4ig.hu>
2020-12-02 13:34:19 +0100

    Template initials.

---
+++ src/main/java/hu/_4ig/springmvchelloworld/SpringMvcHelloworldApplication.java
@@ -0,0 +1,13 @@
+package hu._4ig.springmvchelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcHelloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/_4ig/springmvchelloworld/SpringMvcHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package hu._4ig.springmvchelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
